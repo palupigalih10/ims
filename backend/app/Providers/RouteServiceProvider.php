0ec66fa50fe43a651e274a91bfd2faba2090f542
palupigalih81@gmail.com <palupigalih81@gmail.com>
2024-12-26 16:24:17 +0700

    feat(backend): add route service provider

---
+++ backend/app/Providers/RouteServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Route::middleware('web')
+            ->namespace('App\\Http\\Controllers\\Admin')
+            ->group(base_path('routes/admin.php'));
+    }
+}
