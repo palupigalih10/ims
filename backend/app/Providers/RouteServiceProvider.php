9aefffca75a4ba543540893731623cc5901f18a2
palupigalih81@gmail.com <palupigalih81@gmail.com>
2024-12-26 20:47:03 +0700

    refactor(routes): improve api route organization

--- backend/app/Providers/RouteServiceProvider.php
+++ backend/app/Providers/RouteServiceProvider.php
@@ -8,6 +8,13 @@ use Illuminate\Support\Facades\Route;
 class RouteServiceProvider extends ServiceProvider
 {
     /**
+     * Defined API v1 route path.
+     * 
+     * @var string 
+     */
+    const APIV1_PATH =  'routes/api/v1';
+
+    /**
      * Register services.
      */
     public function register(): void
@@ -20,8 +27,13 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Route::middleware('web')
-            ->namespace('App\\Http\\Controllers\\Admin')
-            ->group(base_path('routes/admin.php'));
+        Route::middleware('api')->prefix('api')->group(function () {
+            // Api V1 routes
+            Route::prefix('v1')->group(function () {
+                // Admin routes
+                Route::namespace('App\\Http\\Controllers\\Admin')
+                    ->group(base_path(self::APIV1_PATH . '/admin.php'));
+            });
+        });
     }
 }
